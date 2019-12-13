c3bd2d76e32c73fa7a567f3191eba854f9f8a155
beejaybee <oyewob@gmail.com>
2019-12-13 11:17:33 +0100

    Updates

---
+++ precache-manifest.423a6d72c3c6f923bbf61870600ba91f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40088fe5c4758ac00e7326643c96a3e",
+    "url": "/robocaptain/index.html"
+  },
+  {
+    "revision": "b22eece7d422dc8f2035",
+    "url": "/robocaptain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a88b9c532ca0342335ae",
+    "url": "/robocaptain/static/css/main.ff50c434.chunk.css"
+  },
+  {
+    "revision": "b22eece7d422dc8f2035",
+    "url": "/robocaptain/static/js/2.3f660bbe.chunk.js"
+  },
+  {
+    "revision": "a88b9c532ca0342335ae",
+    "url": "/robocaptain/static/js/main.3bd608c2.chunk.js"
+  },
+  {
+    "revision": "3a6149704bce5be65ec5",
+    "url": "/robocaptain/static/js/runtime-main.fff7f4eb.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robocaptain/static/media/SEGA.12a3db8b.woff"
+  }
+]);
